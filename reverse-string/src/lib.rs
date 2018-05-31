092560ea80ac2add3b212d0765e93c29dd6894b7
Yusuf Tran <yusuf.tran@karhoo.com>
2018-05-31 07:32:07 +0100

    add reverse-string

---
+++ reverse-string/src/lib.rs
@@ -0,0 +1,10 @@
+extern crate unicode_reverse;
+use unicode_reverse::reverse_grapheme_clusters_in_place;
+
+
+pub fn reverse(input: &str) -> String {
+    let mut x = input.to_string();
+
+    reverse_grapheme_clusters_in_place(&mut x);
+    x
+}
